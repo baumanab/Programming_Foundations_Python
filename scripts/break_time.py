163fd719e5703a3e053152df29850e8a4f4e2138
baumanab <baumanab@gmail.com>
2015-11-21 10:56:22 -0800

    wrap browser and time code in while loop

--- scripts/break_time.py
+++ scripts/break_time.py
@@ -6,10 +6,20 @@ function accepts a time and url and executes opening the url in a
 web browser per unit time.
 '''
 
-# pause for requisite period
 
-time.sleep(10)
 
-# open url with song
+# wrap time and url open code in loop
 
-webbrowser.open('https://www.youtube.com/watch?v=FKscaLcO7ko')
+count = 0
+
+while count < 3:
+    
+    ## pause for requisite period
+
+    time.sleep(10)
+
+    ## open url with song
+
+    webbrowser.open('https://www.youtube.com/watch?v=FKscaLcO7ko')
+
+    count +=1
