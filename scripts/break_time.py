cdbf636c38859033df0eed3458f37e10027d040f
baumanab <baumanab@gmail.com>
2015-11-21 09:49:09 -0800

    starter code for break time function

---
+++ scripts/break_time.py
@@ -0,0 +1,15 @@
+import time
+import webbrowser
+
+'''
+function accepts a time and url and executes opening the url in a
+web browser per unit time.
+'''
+
+# pause for requisite period
+
+time.sleep(10)
+
+# open url with song
+
+webbrowser.open('https://www.youtube.com/watch?v=FKscaLcO7ko')
