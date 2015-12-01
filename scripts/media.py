7c5703da3a69f4e9ba32e5a21ec0aa395d63a34c
baumanab <baumanab@gmail.com>
2015-11-30 21:40:54 -0800

    add class variable for movie ratings, add doc string

--- scripts/media.py
+++ scripts/media.py
@@ -10,6 +10,12 @@ import webbrowser
 
 
 class Movie():
+    '''
+    This class provides a way to store movie attributes and information.
+    '''
+
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
