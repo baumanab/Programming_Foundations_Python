fd00b9cbe571572533f774d48e1e41138c4192e9
baumanab <baumanab@gmail.com>
2015-12-06 23:22:51 -0800

    add show_info method to Parent class

---
+++ scripts/inheritance.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 21:38:45 2015
+
+@author: Andrew
+"""
+
+
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def show_info(self):
+        print("Last Name - {}".format(self.last_name))
+        print("Eye Color - {}".format(self.eye_color))
+
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child constructor called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+# billy_cyrus = Parent("Cyrus", "blue")
+# print(billy_cyrus.last_name)
+# billy_cyrus.show_info()
+
+miley_cyrus = Child("Cyrus", "Blue", 5)
+# print(miley_cyrus.last_name)
+# print(miley_cyrus.number_of_toys)
+miley_cyrus.show_info()
